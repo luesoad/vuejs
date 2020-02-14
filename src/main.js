a4a73620ac643a9fa9274d0976668b8b47eb74fb
Charlotte Jessegus <charlotte.jessegus@dkd.de>
2020-02-14 09:33:33 +0100

    fix router

--- src/main.js
+++ src/main.js
@@ -1,19 +1,13 @@
 import Vue from "vue";
 import App from "./App";
-import VueRouter from "vue-router";
 import "./components";
 require("./sass/main.scss");
-
-Vue.use(VueRouter);
-
-console.log("[i] Starting client app");
 
 Vue.config.productionTip = false;
 
 new Vue({
+  render: h => h(App),
   el: "#app",
   template: "<App/>",
-  render: h => h(App),
-  VueRouter,
   components: { App }
 }).$mount("#app");
