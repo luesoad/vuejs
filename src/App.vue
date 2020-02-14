87071717c98150e97e1036d24b011b56545f72a4
Charlotte Jessegus <charlotte.jessegus@dkd.de>
2020-02-14 10:56:16 +0100

    add basic footer

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,18 @@
   <div id="app">
     <main-navigation />
     <router-view></router-view>
+    <the-footer />
   </div>
 </template>
 
 <script>
 import MainNavigation from "./components/MainNavigation";
+import TheFooter from "./components/TheFooter";
 
 export default {
   name: "App",
   components: {
+    TheFooter,
     MainNavigation
   }
 };
---
+++ src/components/TheFooter.vue
@@ -0,0 +1,21 @@
+<template>
+  <footer>
+    <div class="container">
+      <router-link class="" :to="{ name: 'home', params: {} }">
+        Text
+      </router-link>
+      <span class="attribution">
+        An interactive learning project from
+        <a rel="noopener noreferrer" target="blank" href=""
+          >Lorem </a
+        > Bla
+      </span>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "TheFooter"
+};
+</script>
