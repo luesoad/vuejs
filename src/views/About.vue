370d95dffe07a9eb0494c415a6755ed52ecf452c
Charlotte Jessegus <charlotte.jessegus@dkd.de>
2020-02-27 15:44:37 +0100

    show tabs

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,14 +1,19 @@
 <template>
+  <div>
   <animated-dot></animated-dot>
+  <tabs></tabs>
+  </div>
 </template>
 
 <script>
 import AnimatedDot from "@/components/AnimatedDot.vue";
+import Tabs from "@/components/Tabs.vue";
 
 export default {
   name: "Home",
   components: {
-    AnimatedDot
+    AnimatedDot,
+    Tabs
   }
 };
 </script>
