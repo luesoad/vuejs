76ed2884c3d709dccd0e331fa49f7c87f892732c
Charlotte Jessegus <charlotte.jessegus@dkd.de>
2020-02-27 15:33:06 +0100

    add links to footer

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -1,14 +1,16 @@
 <template>
   <footer>
-    <div class="container">
-      <router-link class="" :to="{ name: 'Home', params: {} }">
-        Text
-      </router-link>
-      <span class="attribution">
-        An interactive learning project from
-        <a rel="noopener noreferrer" target="blank" href="">Lorem</a>Bla
-      </span>
-    </div>
+    <p>This is a playground for vuejs</p>
+    <ul>
+      <li>
+        <router-link class="" :to="{ name: 'Home', params: {} }">
+          Home
+        </router-link>
+      </li>
+      <li>
+        <router-link :to="{ name: 'About' }" exact>About</router-link>
+      </li>
+    </ul>
   </footer>
 </template>
 
