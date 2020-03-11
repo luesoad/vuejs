d843f3ea719a999c7b6553a215227dab4c931cd9
Charlotte Jessegus <charlotte.jessegus@dkd.de>
2020-03-11 16:30:32 +0100

    remove animated dot

--- src/components/AnimatedDot.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div class="intro-banner-vdo-play-btn pinkBg">
-    <span class="ripple pinkBg"></span>
-    <span class="ripple pinkBg"></span>
-    <span class="ripple pinkBg"></span>
-  </div>
-</template>
