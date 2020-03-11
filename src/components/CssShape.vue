1bbb002e33163bc88f319c25d3cf83a5c53ce774
Charlotte Jessegus <charlotte.jessegus@dkd.de>
2020-03-11 16:23:27 +0100

    css shapes restructuring + responsive

--- src/components/CssShape.vue
+++ src/components/CssShape.vue
@@ -1,14 +1,22 @@
 <template>
   <div class="css-shape">
     <div class="css-shape__left">
-  <div class="triangle-top-left__big"></div>
-  <div class="triangle-top-left__small"></div>
-  <div class="circle-top-left"></div>
+      <div
+        class="css-shape__triangle__big css-shape__triangle__big--left"
+      ></div>
+      <div
+        class="css-shape__triangle__small css-shape__triangle__small--left"
+      ></div>
+      <div class="css-shape__circle css-shape__circle--left"></div>
     </div>
     <div class="css-shape__right">
-      <div class="triangle-top-right__big"></div>
-      <div class="triangle-top-right__small"></div>
-      <div class="circle-top-right"></div>
+      <div
+        class="css-shape__triangle__big css-shape__triangle__big--right"
+      ></div>
+      <div
+        class="css-shape__triangle__small css-shape__triangle__small--right"
+      ></div>
+      <div class="css-shape__circle css-shape__circle--right"></div>
     </div>
   </div>
 </template>
