0bc659b0d4680e0934dc350d2cdfe23b608b09ee
Charlotte Jessegus <charlotte.jessegus@dkd.de>
2020-03-11 15:49:45 +0100

    add another pair of shapes

--- src/components/CssShape.vue
+++ src/components/CssShape.vue
@@ -5,5 +5,10 @@
   <div class="triangle-top-left__small"></div>
   <div class="circle-top-left"></div>
     </div>
+    <div class="css-shape__right">
+      <div class="triangle-top-right__big"></div>
+      <div class="triangle-top-right__small"></div>
+      <div class="circle-top-right"></div>
+    </div>
   </div>
 </template>
